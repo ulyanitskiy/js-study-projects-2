c8e6a1919d35fec2443cc5388a36e2e1878a93ec
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-09-03 15:58:25 +0300

    HW17 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item) {
+  const index = array.indexOf(item);
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
+}
+
+removeElement(array, 5);
+console.log(array);
