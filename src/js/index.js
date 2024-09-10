62f92d7fa5d562a84ebf2c3bab8e96348ffdc4bf
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-09-10 12:39:53 +0300

    HW21 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const length = prompt('Введіть довжину масиву:');
+const array = [];
+for (let i = 0; i < length; i++) {
+  array.push(Number(prompt(`Введіть елемент ${i + 1}:`)));
+}
+console.log('Початковий масив:', array);
+
+array.sort((a, b) => a - b);
+console.log('Відсортований масив:', array);
+
+array.splice(1, 3);
+console.log('Масив після видалення елементів з 2 до 4:', array);
+
+console.log('Кінцевий масив:', array);
