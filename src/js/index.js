5035727f6efa0e3c83b68139b60a8b7321ecbea6
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-09-23 17:25:56 +0300

    HW27 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const customBind = (fn, context) => (...args) => fn.bind(context)(...args);
+
+const obj = {
+  x: 2,
+};
+
+function getX() {
+  return this.x;
+}
+
+const boundGetX = customBind(getX, obj);
+console.log(boundGetX());
