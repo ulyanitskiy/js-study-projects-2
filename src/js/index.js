7bfdbceedd93eb4ae81348bb682fe271e5c3d84b
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-09-23 22:43:26 +0300

    HW30 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const accumulateSum = () => {
+  let currentSum = 0;
+
+  return (value) => {
+    currentSum += value;
+    return currentSum;
+  };
+};
+
+const addValue = accumulateSum();
+
+console.log(addValue(3));
+console.log(addValue(5));
+console.log(addValue(20));
