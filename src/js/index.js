b1b2106cbe61fca79b1928a0b288d52134d54171
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-03 00:32:11 +0300

    HW33 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const getRandomImage = () => {
+  const randomNumber = Math.floor(Math.random() * 9) + 1;
+  const img = document.createElement('img');
+  img.src = `./images/${randomNumber}.jpg`;
+  img.alt = `Image ${randomNumber}`;
+  img.title = `Image ${randomNumber}`;
+  document.body.appendChild(img);
+};
+getRandomImage();
