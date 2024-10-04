bd19b34ffe810f81b4b82c561f979a09d608cac9
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-04 23:18:30 +0300

    HW33 updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const getRandomImage = () => {
-  const randomNumber = Math.floor(Math.random() * 9) + 1;
+const getRandomNumberInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const insertRandomImage = () => {
+  const randomNumber = getRandomNumberInRange(1, 9);
   const img = document.createElement('img');
   img.src = `./images/${randomNumber}.jpg`;
   img.alt = `Image ${randomNumber}`;
   img.title = `Image ${randomNumber}`;
   document.body.appendChild(img);
 };
-getRandomImage();
+
+insertRandomImage();
