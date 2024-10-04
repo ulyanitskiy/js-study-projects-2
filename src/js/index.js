a48ca976ee57269170cd6ce9a80ba6e12600baba
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-05 00:26:55 +0300

    HW34 updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const generateElements = () => {
+  const inputField = document.createElement('input');
+  inputField.type = 'text';
+  inputField.id = 'inputField';
+  inputField.placeholder = 'Наведіть фокус на мене';
+
+  const infoBox = document.createElement('div');
+  infoBox.id = 'infoBox';
+  infoBox.textContent = 'Зробив домашку?';
+
+  return { inputField, infoBox };
+};
+
+const insertElements = (inputField, infoBox) => {
+  document.body.appendChild(inputField);
+  document.body.appendChild(infoBox);
+};
+
+const { inputField, infoBox } = generateElements();
+insertElements(inputField, infoBox);
+
+inputField.addEventListener('focus', () => {
+  infoBox.style.display = 'block';
+});
+
+inputField.addEventListener('focusout', () => {
+  infoBox.style.display = 'none';
+});
