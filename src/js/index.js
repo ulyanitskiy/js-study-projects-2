303ccb20b572e040b7eea8a55cac515581089b9a
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-04 22:46:10 +0300

    HW32 updated

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,29 @@
 'use strict';
 
-const container = document.getElementById('table-container');
-const table = document.createElement('table');
-let number = 1;
+const countNumbers = () => {
+  let currentNumber = 1;
+  return () => currentNumber++;
+};
 
-for (let i = 0; i < 10; i++) {
-  const row = document.createElement('tr');
-  for (let j = 0; j < 10; j++) {
-    const cell = document.createElement('td');
-    cell.textContent = number;
-    if (number % 2 === 0) {
-      cell.style.backgroundColor = 'lightcyan';
-    } else {
-      cell.style.backgroundColor = 'lavender';
+const createTable = (rows, columns, counter) => {
+  const table = document.createElement('table');
+
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < columns; j++) {
+      const cell = document.createElement('td');
+      const number = counter();
+      cell.textContent = number;
+      cell.style.backgroundColor = (number % 2 === 0) ? 'lightcyan' : 'lavender';
+      row.appendChild(cell);
     }
-    row.appendChild(cell);
-    number++;
+    table.appendChild(row);
   }
-  table.appendChild(row);
-}
+  return table;
+};
 
-container.appendChild(table);
+const insertTable = (container, table) => container.appendChild(table);
+const container = document.getElementById('table-container');
+const counter = countNumbers();
+const table = createTable(10, 10, counter);
+insertTable(container, table);
