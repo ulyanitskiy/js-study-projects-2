171e089509aaa31845f095ae2ec50470f07ba6b0
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-08 17:46:22 +0300

    HW35 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const elementsList = document.querySelectorAll('#ulId li');
+elementsList.forEach((element) => console.log(element.textContent));
+
+const ulElement = document.querySelector('#ulId');
+const attributesArray = [];
+for (let attr of ulElement.attributes) {
+  attributesArray.push(attr.value);
+}
+console.log(attributesArray);
+
+const attributeNamesArray = [];
+for (let attr of ulElement.attributes) {
+  attributeNamesArray.push(attr.name);
+}
+console.log(attributeNamesArray);
+
+ulElement.lastElementChild.textContent = 'Hi, my name is Alex!';
+
+const firstLi = ulElement.firstElementChild;
+firstLi.setAttribute('data-my-name', 'Alex');
+
+ulElement.removeAttribute('data-dog-tail');
