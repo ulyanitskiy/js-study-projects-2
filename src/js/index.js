dd63e52be34caad6d3db31e6ba8ff6b64eccc519
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-11 18:06:13 +0300

    HW36 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function generateList(array) {
+  const ul = document.createElement('ul');
+  array.forEach((item) => {
+    const li = document.createElement('li');
+
+    if (Array.isArray(item)) {
+      li.appendChild(generateList(item));
+    } else {
+      li.textContent = item;
+    }
+
+    ul.appendChild(li);
+  });
+  return ul;
+}
+
+const array1 = [1, 2, 3];
+document.body.appendChild(generateList(array1));
+const array2 = [1, 2, [1.1, 1.2, 1.3], 3];
+document.body.appendChild(generateList(array2));
