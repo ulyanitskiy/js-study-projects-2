908bc4868abfbfdfdd93ca8d6b6dd59bd8ca54f2
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-10-24 19:00:34 +0300

    HW39 completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+let user = {
+  data: {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: {
+      a1: 1,
+      b1: 2,
+      c1: 3,
+      d1: {
+        a2: 3,
+        b2: 3,
+        c2: 3,
+      },
+    },
+  },
+};
+
+const deepFreeze = (obj) => {
+  Object.freeze(obj);
+
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === 'object' && obj[key] !== null && !Object.isFrozen(obj[key])) {
+      deepFreeze(obj[key]);
+    }
+  });
+
+  return obj;
+};
+
+deepFreeze(user);
+
+if (!Object.isFrozen(user.data)) {
+  user.data.a = 10;
+} else {
+  console.log("Об'єкт user.data заморожений, зміна неможлива");
+}
+
+console.log(user.data.a);
