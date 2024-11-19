7599b386d4031512b058c947bb0921425cf1564d
Alexey Ulyanitskiy <theulyanitskiy@gmail.com>
2024-11-19 16:03:20 +0200

    HW44 completed

--- Readme.md
+++ Readme.md
@@ -1,1 +1,39 @@
 # Task
+
+Розшир задачі зі спадкуванням новими функціями та логікою:
+
+Загальні вимоги:
+Singleton:
+Клас EmailService повинен бути реалізований як Singleton (тільки один екземпляр може бути створений для базового класу).
+
+Обмеження преміум email:
+У класі PremiumEmailService встанови обмеження: не більше 5 преміум email. Якщо ліміт перевищено, метод addPremiumEmail повинен кидати помилку.
+
+Взаємодія між екземплярами:
+Додай клас EnterpriseEmailService, який також успадковує EmailService.
+EnterpriseEmailService повинен мати метод migratePremiumEmails(targetService), який переносить всі преміум email до іншого екземпляра PremiumEmailService.
+
+Додаткові функції:
+Кожен клас має логіку логування (через захищений метод _log(message)), який додає запис в масив приватної властивості #logs та виводить його в консоль.
+Створи метод getLogs() для отримання всіх записів логів.
+
+Приклад використання
+
+const premiumService1 = new PremiumEmailService();
+const premiumService2 = new PremiumEmailService();
+const enterpriseService = new EnterpriseEmailService();
+
+premiumService1.addPremiumEmail("vip1@premium.com");
+premiumService1.addPremiumEmail("vip2@premium.com");
+
+console.log(premiumService1.getPremiumEmails());
+// ["vip1@premium.com", "vip2@premium.com"]
+
+enterpriseService.addPremiumEmail("enterprise@premium.com");
+
+enterpriseService.migratePremiumEmails(premiumService2);
+
+console.log(premiumService2.getPremiumEmails());
+// ["enterprise@premium.com"]
+
+console.log(premiumService1.getLogs());
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,75 @@
-'use strict';
+class EmailService {
+    static instance = null;
+    _logs = [];
+
+    constructor() {
+        if (EmailService.instance) {
+            return EmailService.instance;
+        }
+        this._premiumEmails = [];
+        EmailService.instance = this;
+    }
+
+    addPremiumEmail(email) {
+        if (this._premiumEmails.length >= 5) {
+            this._log('Error: Cannot add more than 5 premium emails.');
+            throw new Error('Cannot add more than 5 premium emails.');
+        }
+        this._premiumEmails.push(email);
+        this._log(`Added premium email: ${email}`);
+    }
+
+    get premiumEmails() {
+        return this._premiumEmails;
+    }
+
+    _log(message) {
+        this._logs.push(message);
+        console.log(message);
+    }
+
+    get logs() {
+        return this._logs;
+    }
+}
+
+class PremiumEmailService extends EmailService {
+    constructor() {
+        super();
+        if (PremiumEmailService.instance) {
+            return PremiumEmailService.instance;
+        }
+        PremiumEmailService.instance = this;
+    }
+}
+
+class EnterpriseEmailService extends EmailService {
+    constructor() {
+        super();
+    }
+
+    migratePremiumEmails(targetService) {
+        if (!(targetService instanceof PremiumEmailService)) {
+            this._log('Error: Target service must be an instance of PremiumEmailService.');
+            throw new Error('Target service must be an instance of PremiumEmailService.');
+        }
+        targetService._premiumEmails = targetService._premiumEmails.concat(this._premiumEmails);
+        this._premiumEmails = [];
+        this._log('Migrated premium emails to target service.');
+    }
+}
+
+const premiumService1 = new PremiumEmailService();
+const premiumService2 = new PremiumEmailService();
+const enterpriseService = new EnterpriseEmailService();
+
+premiumService1.addPremiumEmail('vip1@premium.com');
+premiumService1.addPremiumEmail('vip2@premium.com');
+
+console.log(premiumService1.premiumEmails);
+
+enterpriseService.addPremiumEmail('enterprise@premium.com');
+enterpriseService.migratePremiumEmails(premiumService2);
+
+console.log(premiumService2.premiumEmails);
+console.log(premiumService1.logs);
